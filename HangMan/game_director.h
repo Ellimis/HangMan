58587b17e60ef77fb8b60a4c329c74a9422c303f
Ellimis <Ellimis@github.com>
2020-07-04 23:36:06 +0900

    add game_director.h

---
+++ HangMan/game_director.h
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class GameDirector {
+	vector<string> vocabulary; // word vocabulary
+public:
+	GameDirector();
+	void append_word(ifstream &fin);
+	string encode_word(string word);
+	void game();
+};
+
+GameDirector::GameDirector() {
+	cout << "----------------------------------------" << endl;
+	cout << "|    Let's start the Hangman game !    |" << endl;
+	cout << "----------------------------------------" << endl;
+}
+
+// 193 words
+void GameDirector::append_word(ifstream &fin) { // word append from txt file to vector
+	string word;
+
+	while (getline(fin, word))
+		this->vocabulary.push_back(word);
+
+	fin.close();
+}
+
+string GameDirector::encode_word(string word) { // encode word for the game
+	const int MAX_HiddenWord = 2; // Max number of char to be hidden
+
+	for (int i = 0; i < MAX_HiddenWord; i++) {
+		char hide = word[rand() % word.size()];
+
+		if (hide == '-') {
+			i--;
+			continue;
+		}
+
+		for (int j = 0; j < word.size(); j++) {
+			if (word[j] == hide) {
+				word[j] = '-';
+			}
+		}
+	}
+
+	return word;
+}
+
+void GameDirector::game() {
+	string word = this->vocabulary[rand() % 193]; // 0 ~ 193 random word, for indexing in vector
+	string problem;
+
+	problem = this->encode_word(word);
+
+	for (int i = 0; i < 5; i++) {
+		bool find = false;
+		char input;
+		cout << problem << endl;
+		cin >> input;
+
+		for (int j = 0; j < word.size(); j++) {
+			if (word[j] == input && problem[j] == '-') {
+				problem[j] = input;
+				find = true;
+			}
+		}
+
+		if (find) i--; // if find one letter then give one more chance (total 5 failes will be defeat)
+		if (!word.compare(problem)) { // return 0 if two objects are the same.
+			cout << "word : " << problem << endl;
+			return;
+		}
+	}
+
+	cout << "Failed to complete word in 5 chances." << endl;
+	cout << "word : " << word << endl;
+}
--- HangMan/main.cpp
+++ HangMan/main.cpp
@@ -1,94 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <ctime> // for seed
+#include "game_director.h"
 
 using namespace std;
 
-// 193 words
-void open_file(vector<string> &v) {
+int main() {
 	const char* file_loc = "C:/words.txt";
-	ifstream is(file_loc);
-	string word;
+	ifstream inFile(file_loc);
 
-	if (!is) {
+	if (!inFile) {
 		cerr << "ERROR : Can't open file";
 		exit(0);
 	}
 
-	while (getline(is, word))
-		v.push_back(word);
+	GameDirector director;
 
-	is.close();
-}
+	director.append_word(inFile);
 
-string encode_word(string word) {
-	const int MAX_HiddenWord = 2; // Max number of char to be hidden
-
-	for (int i = 0; i < MAX_HiddenWord; i++) {
-		char hide = word[rand() % word.size()];
-
-		if (hide == '-') {
-			i--;
-			continue;
-		}
-
-		for (int j = 0; j < word.size(); j++) {
-			if (word[j] == hide) {
-				word[j] = '-';
-			}
-		}
-	}
-
-	return word;
-}
-
-void game(vector<string> &v) {
-	int wordLoc = rand() % 193; // 0 ~ 193 random number, for indexing in vector
-	string word = v[wordLoc]; // random word
-	string problem;
-
-	problem = encode_word(word);
-	
-	for (int i = 0; i < 5; i++) {
-		bool find = false;
-		char input;
-		cout << problem << endl;
-		cin >> input;
-		
-		for (int j = 0; j < word.size(); j++) {
-			if (word[j] == input && problem[j] == '-') {
-				problem[j] = input;
-				find = true;
-			}
-		}
-		
-		if (find) i--; // if find one letter then give one more chance (total 5 failes will be defeat)
-		if (!word.compare(problem)) { // return 0 if two objects are the same.
-			cout << problem << endl;
-			return;
-		}
-	}
-
-	cout << "Failed to complete word in 5 chances." << endl;
-	cout << word << endl;
-}
-
-int main() {
-	vector<string> voca;
-	srand((unsigned) time(0)); // for rand
-
-	open_file(voca);
-
-	cout << "----------------------------------------" << endl;
-	cout << "|    Let's start the Hangman game !    |" << endl;
-	cout << "----------------------------------------" << endl;
-
+	srand((unsigned)time(0)); // for rand
 	char retry;
 	do {
-		game(voca);
-		cout << "Next(y/n)?" << endl;
+		director.game();
+		cout << "Next(y/n)? : ";
 		cin >> retry;
 	} while (retry == 'y');
 
