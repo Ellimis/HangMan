f8c327ac5eb2d83aa72db592c05809e14de05e92
Ellimis <Ellimis@github.com>
2020-07-04 20:39:01 +0900

    open file for game words

--- HangMan/main.cpp
+++ HangMan/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main() {
+// 193 words
+void open_file(vector<string> &v) {
 	const char* file_loc = "C:/words.txt";
 	ifstream is(file_loc);
+	string word;
+
 	if (!is) {
 		cerr << "ERROR : Can't open file";
 		exit(0);
 	}
-	string word;
-	while (getline(is, word)) {
 
-	}
+	while (getline(is, word))
+		v.push_back(word);
+
+	is.close();
+}
+
+int main() {
+	vector<string> voca;
+
+	open_file(voca);
+
+	//for (int i = 0; i < voca.size(); i++)
+	//	cout << voca[i] << ' ' << i << endl;
 
 	return 0;
 }
